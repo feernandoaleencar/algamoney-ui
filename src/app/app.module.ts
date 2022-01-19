281a3433b33808edaca87398c5766fad4473f245
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-19 00:03:11 -0300

    Criando layout de pesquisa de lançamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {InputTextModule} from "primeng/inputtext";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        InputTextModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
