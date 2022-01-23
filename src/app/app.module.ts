a63fca59d3fc58aee5fd5bd8d204a73b32af1d16
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-23 00:17:15 -0300

    Adicionando seletor de data (componente Calendar) de cadastro de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
 import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
 import {TableModule} from "primeng/table";
 import {TooltipModule} from "primeng/tooltip";
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PessoasPesquisaComponent } from './pessoas-pesquisa/pessoas-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
 import {InputTextareaModule} from "primeng/inputtextarea";
+import {CalendarModule} from "primeng/calendar";
+
+import {AppComponent} from './app.component';
+import {LancamentosPesquisaComponent} from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PessoasPesquisaComponent} from './pessoas-pesquisa/pessoas-pesquisa.component';
+import {LancamentoCadastroComponent} from './lancamento-cadastro/lancamento-cadastro.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LancamentosPesquisaComponent,
-    NavbarComponent,
-    PessoasPesquisaComponent,
-    LancamentoCadastroComponent
-  ],
+    declarations: [
+        AppComponent,
+        LancamentosPesquisaComponent,
+        NavbarComponent,
+        PessoasPesquisaComponent,
+        LancamentoCadastroComponent
+    ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
         InputTextModule,
         ButtonModule,
         TableModule,
         TooltipModule,
-        InputTextareaModule
+        InputTextareaModule,
+        CalendarModule
     ],
-  providers: [],
-  bootstrap: [AppComponent]
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
