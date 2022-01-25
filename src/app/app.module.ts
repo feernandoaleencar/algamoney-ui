256788931981f61aa19bd740ee55af90d080a306
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-24 22:09:42 -0300

    Criando o Core Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,13 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 import {AppComponent} from './app.component';
-import {NavbarComponent} from './navbar/navbar.component';
 import {LancamentosModule} from "./lancamentos/lancamentos.module";
 import {PessoasModule} from "./pessoas/pessoas.module";
+import {CoreModule} from "./core/core.module";
 
 @NgModule({
     declarations: [
         AppComponent,
-        NavbarComponent,
     ],
     imports: [
         BrowserModule,
@@ -19,7 +18,7 @@ import {PessoasModule} from "./pessoas/pessoas.module";
         LancamentosModule,
         PessoasModule,
 
-
+        CoreModule
     ],
     providers: [],
     bootstrap: [AppComponent]
