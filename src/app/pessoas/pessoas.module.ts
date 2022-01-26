978dc199b42599a1a91187a5a7153ef9878cf226
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-01-25 22:42:53 -0300

    Criando componente para pessoas-grid

--- src/app/pessoas/pessoas.module.ts
+++ src/app/pessoas/pessoas.module.ts
@@ -1,7 +1,12 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {FormsModule} from "@angular/forms";
+
 import {PessoaCadastroComponent} from "./pessoa-cadastro/pessoa-cadastro.component";
 import {PessoasPesquisaComponent} from "./pessoas-pesquisa/pessoas-pesquisa.component";
+import {PessoasGridComponent} from './pessoas-grid/pessoas-grid.component';
+import {SharedModule} from "../shared/shared.module";
+
 import {InputTextModule} from "primeng/inputtext";
 import {ButtonModule} from "primeng/button";
 import {TableModule} from "primeng/table";
@@ -12,14 +17,12 @@ import {SelectButtonModule} from "primeng/selectbutton";
 import {DropdownModule} from "primeng/dropdown";
 import {InputNumberModule} from "primeng/inputnumber";
 import {InputMaskModule} from "primeng/inputmask";
-import {FormsModule} from "@angular/forms";
-import {SharedModule} from "../shared/shared.module";
-
 
 @NgModule({
     declarations: [
         PessoaCadastroComponent,
-        PessoasPesquisaComponent
+        PessoasPesquisaComponent,
+        PessoasGridComponent
     ],
     imports: [
         CommonModule,
