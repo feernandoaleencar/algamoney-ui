43b652aa5aebe88ff64a975240fd89683e1e644c
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-07 16:39:54 -0300

    Adicionando filtro por descrição na pesquisa de lançamentos

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -1,5 +1,9 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpHeaders } from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+
+export interface lancamentoFiltro {
+    descricao: string;
+}
 
 @Injectable({
     providedIn: 'root'
@@ -13,10 +17,18 @@ export class LancamentoService {
     ) {
     }
 
-    pesquisar(): Promise<any> {
+    pesquisar(filtro: lancamentoFiltro): Promise<any> {
+
         const headers = new HttpHeaders().append('Authorization', 'Basic YWRtaW5AYWxnYW1vbmV5LmNvbTphZG1pbg==');
 
-        return this.http.get(`${this.lancamentosUrl}?resumo`,{ headers })
+        let params = new HttpParams();
+
+        if (filtro.descricao) {
+           params = params.set('descricao', filtro.descricao);
+        }
+
+        return this.http.get(`${this.lancamentosUrl}?resumo`,
+            { headers, params})
             .toPromise()
             .then((response : any) => response['content']);
 
