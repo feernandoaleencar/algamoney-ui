8bf02078c035d026f20a3d59aa46518111f94375
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-07 16:08:31 -0300

    Criando o serviço de consulta de lançamentos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import {AppComponent} from './app.component';
 import {LancamentosModule} from "./lancamentos/lancamentos.module";
 import {PessoasModule} from "./pessoas/pessoas.module";
 import {CoreModule} from "./core/core.module";
+import {HttpClientModule} from "@angular/common/http";
+import {LancamentoService} from "./lancamentos/lancamento.service";
 
 @NgModule({
     declarations: [
@@ -18,9 +20,10 @@ import {CoreModule} from "./core/core.module";
         LancamentosModule,
         PessoasModule,
 
-        CoreModule
+        CoreModule,
+        HttpClientModule
     ],
-    providers: [],
+    providers: [LancamentoService],
     bootstrap: [AppComponent]
 })
 export class AppModule {
