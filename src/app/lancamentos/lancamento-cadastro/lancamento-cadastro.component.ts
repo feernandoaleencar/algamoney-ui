03f211b85c3f79aa28eaa7088ac3b2f9a0ce4dfd
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-09 16:38:27 -0300

    Listando as categorias cadastradas no dropdown

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -1,32 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoriaService} from "../../categoria/categoria.service";
+import {ErrorHandlerService} from "../../core/error-handler.service";
 
 @Component({
-  selector: 'app-lancamento-cadastro',
-  templateUrl: './lancamento-cadastro.component.html',
-  styleUrls: ['./lancamento-cadastro.component.css']
+    selector: 'app-lancamento-cadastro',
+    templateUrl: './lancamento-cadastro.component.html',
+    styleUrls: ['./lancamento-cadastro.component.css']
 })
 export class LancamentoCadastroComponent implements OnInit {
 
     tipos = [
-        { label: 'Receita', value: 'RECEITA'},
-        { label: 'Despesa', value: 'DESPESA'},
+        {label: 'Receita', value: 'RECEITA'},
+        {label: 'Despesa', value: 'DESPESA'},
     ];
 
-    categorias = [
-        { label: 'Alimentação', value: 1 },
-        { label: 'Transporte', value: 2 },
-    ];
+    categorias: any = [];
 
     pessoas = [
-        { label: 'João da Silva', value: 1 },
-        { label: 'Sebastião Souza', value: 2 },
-        { label: 'Maria Abadia', value: 3 },
+        {label: 'João da Silva', value: 1},
+        {label: 'Sebastião Souza', value: 2},
+        {label: 'Maria Abadia', value: 3},
     ];
 
 
-  constructor() { }
+    constructor(
+        private categoriaService: CategoriaService,
+        private errorHandlerService: ErrorHandlerService
+    ) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.carregarCategorias();
+    }
+
+    carregarCategorias() {
+        return this.categoriaService.listarCategorias()
+            .then(categorias => {
+                this.categorias = categorias.map((c: any) => ({ label: c.nome, value: c.id }))
+            })
+            .catch(erro => this.errorHandlerService.handle(erro));
+    }
 
 }
