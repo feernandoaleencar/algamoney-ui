a8e1b1a4acf4ba5ef2665829273adb82efd658f6
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-10 20:55:57 -0300

    Configurando rotas na aplicação

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,21 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {HttpClientModule} from "@angular/common/http";
+import {RouterModule, Routes} from "@angular/router";
+import {CoreModule} from "./core/core.module";
 
 import {AppComponent} from './app.component';
 import {LancamentosModule} from "./lancamentos/lancamentos.module";
 import {PessoasModule} from "./pessoas/pessoas.module";
-import {CoreModule} from "./core/core.module";
+import {LancamentosPesquisaComponent} from "./lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component";
+import {LancamentoCadastroComponent} from "./lancamentos/lancamento-cadastro/lancamento-cadastro.component";
+import {PessoasPesquisaComponent} from "./pessoas/pessoas-pesquisa/pessoas-pesquisa.component";
 
+const routes: Routes = [
+    {path: 'lancamentos', component: LancamentosPesquisaComponent},
+    {path: 'lancamentos/novo', component: LancamentoCadastroComponent},
+    {path: 'pessoas', component: PessoasPesquisaComponent},
+];
 
 @NgModule({
     declarations: [
@@ -22,6 +31,7 @@ import {CoreModule} from "./core/core.module";
 
         CoreModule,
         HttpClientModule,
+        RouterModule.forRoot(routes),
     ],
     providers: [
     ],
