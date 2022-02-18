f2c9dbf792b4482446d02074f47913e93e12c4b6
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-18 15:58:31 -0300

    Ajustanto links para os components de pessoas

--- src/app/pessoas/pessoas-routing.module.ts
+++ src/app/pessoas/pessoas-routing.module.ts
@@ -2,9 +2,11 @@ import {NgModule} from "@angular/core";
 
 import {RouterModule, Routes} from "@angular/router";
 import {PessoasPesquisaComponent} from "./pessoas-pesquisa/pessoas-pesquisa.component";
+import {PessoaCadastroComponent} from "./pessoa-cadastro/pessoa-cadastro.component";
 
 const routes: Routes = [
     {path: 'pessoas', component: PessoasPesquisaComponent},
+    {path: 'pessoas/nova', component: PessoaCadastroComponent},
 ];
 
 @NgModule({
