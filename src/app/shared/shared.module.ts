cccd45804ebaf214aa46b50415d7b477d9acadd8
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-23 16:07:59 -0300

    Adicionando components primeng para shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,17 @@ import {CommonModule} from '@angular/common';
 
 import {MessageComponent} from "./message/message.component";
 import {MessageModule} from "primeng/message";
+import {InputTextModule} from "primeng/inputtext";
+import {ButtonModule} from "primeng/button";
+import {TableModule} from "primeng/table";
+import {TooltipModule} from "primeng/tooltip";
+import {InputTextareaModule} from "primeng/inputtextarea";
+import {CalendarModule} from "primeng/calendar";
+import {SelectButtonModule} from "primeng/selectbutton";
+import {DropdownModule} from "primeng/dropdown";
+import {InputNumberModule} from "primeng/inputnumber";
+import {InputMaskModule} from "primeng/inputmask";
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
     declarations: [
@@ -10,12 +21,40 @@ import {MessageModule} from "primeng/message";
     ],
     imports: [
         CommonModule,
+        FormsModule,
 
-        MessageModule
+        MessageModule,
+
+        //componentes primeng
+        InputTextModule,
+        ButtonModule,
+        TableModule,
+        TooltipModule,
+        InputTextareaModule,
+        CalendarModule,
+        SelectButtonModule,
+        DropdownModule,
+        InputNumberModule,
+        InputMaskModule,
 
     ],
     exports: [
-        MessageComponent
+        CommonModule,
+        FormsModule,
+
+        MessageComponent,
+
+        //componentes primeng
+        InputTextModule,
+        ButtonModule,
+        TableModule,
+        TooltipModule,
+        InputTextareaModule,
+        CalendarModule,
+        SelectButtonModule,
+        DropdownModule,
+        InputNumberModule,
+        InputMaskModule,
     ]
 })
 export class SharedModule {
