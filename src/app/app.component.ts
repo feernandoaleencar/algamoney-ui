e36f2ce5c16e3a04539db1663c50a4f33951066e
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-24 15:16:46 -0300

    Metodo para não exibir o navbar na tela de login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {PrimeNGConfig} from "primeng/api";
 import {TranslateService} from "@ngx-translate/core";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'app-root',
@@ -10,12 +11,17 @@ import {TranslateService} from "@ngx-translate/core";
 export class AppComponent implements OnInit{
     constructor(
         private config: PrimeNGConfig,
-        private translateService: TranslateService
+        private translateService: TranslateService,
+        private router: Router
     ) {}
 
     ngOnInit() {
         this.translateService.setDefaultLang('pt-br');
         this.translateService.get('primeng')
             .subscribe(res => this.config.setTranslation(res));
+    }
+
+    exibindoNavbar(){
+        return this.router.url !== '/login';
     }
 }
