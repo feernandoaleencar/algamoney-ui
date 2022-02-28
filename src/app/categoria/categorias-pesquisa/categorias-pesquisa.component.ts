ba30bf2f967176db0b3c17b27a195eb34da54dcf
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-02-28 20:41:38 -0300

    mock de lista de categorias

--- src/app/categoria/categorias-pesquisa/categorias-pesquisa.component.ts
+++ src/app/categoria/categorias-pesquisa/categorias-pesquisa.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-categorias-pesquisa',
-  templateUrl: './categorias-pesquisa.component.html',
-  styleUrls: ['./categorias-pesquisa.component.css']
+    selector: 'app-categorias-pesquisa',
+    templateUrl: './categorias-pesquisa.component.html',
+    styleUrls: ['./categorias-pesquisa.component.css']
 })
 export class CategoriasPesquisaComponent implements OnInit {
 
-  constructor() { }
+    categorias = [
+        {
+            "id": 1,
+            "nome": "Lazer"
+        },
+        {
+            "id": 2,
+            "nome": "Alimentação"
+        },
+        {
+            "id": 3,
+            "nome": "Supermercado"
+        },
+        {
+            "id": 4,
+            "nome": "Farmácia"
+        },
+        {
+            "id": 5,
+            "nome": "Outros"
+        }
+    ]
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
 
 }
