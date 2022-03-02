cb4e995043123ca80a31c82108896e8f11680bd6
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-02 20:34:19 -0300

    Chamando serviço para adicionar categoria

--- src/app/categorias/categoria-cadastro/categoria-cadastro.component.ts
+++ src/app/categorias/categoria-cadastro/categoria-cadastro.component.ts
@@ -2,7 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {NgForm} from "@angular/forms";
 import {ActivatedRoute, Router} from "@angular/router";
 import {Title} from "@angular/platform-browser";
-import {Categoria, Pessoa} from "../../core/model";
+import {Categoria} from "../../core/model";
+import {CategoriaService} from "../categoria.service";
+import {MessageService} from "primeng/api";
+import {ErrorHandlerService} from "../../core/error-handler.service";
 
 @Component({
     selector: 'app-categoria-cadastro',
@@ -14,9 +17,12 @@ export class CategoriaCadastroComponent implements OnInit {
     categoria = new Categoria();
 
     constructor(
+        private categoriaService: CategoriaService,
         private route: ActivatedRoute,
         private router: Router,
-        private title: Title
+        private title: Title,
+        private messageService: MessageService,
+        private errorHandlerService: ErrorHandlerService
     ) { }
 
     ngOnInit(): void {
@@ -25,7 +31,13 @@ export class CategoriaCadastroComponent implements OnInit {
     }
 
     salvar(categoriaForm: NgForm) {
+        this.categoriaService.adicionar(this.categoria)
+            .then(categoria => {
+                this.messageService.add({severity: 'success', detail: 'Categoria adicionada com sucesso!'});
 
+                this.router.navigate(['/categorias', categoria.id])
+            })
+            .catch(erro => this.errorHandlerService.handle(erro));
     }
 
     get editando() {
