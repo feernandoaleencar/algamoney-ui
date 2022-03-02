7bd488d2a9da4bc26d7a272821e0d31781c5abee
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-02 14:38:26 -0300

    Criando rota para componente de cadastro de categoria

--- src/app/categorias/categorias-routing.module.ts
+++ src/app/categorias/categorias-routing.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {CategoriasPesquisaComponent} from "./categorias-pesquisa/categorias-pesquisa.component";
+import {CategoriaCadastroComponent} from "./categoria-cadastro/categoria-cadastro.component";
 
 const routes: Routes = [
     {path: 'categorias', component: CategoriasPesquisaComponent},
+    {path: 'categorias/nova', component: CategoriaCadastroComponent},
 ];
 
 @NgModule({
