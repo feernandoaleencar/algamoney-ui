bee25b363522b5472526f4c81d0bb8454ba86e37
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-02 14:25:47 -0300

    Criando componente de cadastro de categoria

--- src/app/categoria/categorias.module.ts
+++ src/app/categoria/categorias.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from "../shared/shared.module";
 import {CategoriasPesquisaComponent} from "./categorias-pesquisa/categorias-pesquisa.component";
 import {CategoriasRoutingModule} from "./categorias-routing.module";
+import { CategoriaCadastroComponent } from './categoria-cadastro/categoria-cadastro.component';
 
 @NgModule({
     declarations: [
-        CategoriasPesquisaComponent
+        CategoriasPesquisaComponent,
+        CategoriaCadastroComponent
     ],
     imports: [
         SharedModule,
