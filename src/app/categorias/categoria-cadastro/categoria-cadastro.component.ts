bd5d50a8e40a8c6846fbe7cba8b59fc050039619
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-02 20:22:43 -0300

    Criando protótipo para cadastro de categoria

--- src/app/categorias/categoria-cadastro/categoria-cadastro.component.ts
+++ src/app/categorias/categoria-cadastro/categoria-cadastro.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {Title} from "@angular/platform-browser";
+import {Categoria, Pessoa} from "../../core/model";
 
 @Component({
-  selector: 'app-categoria-cadastro',
-  templateUrl: './categoria-cadastro.component.html',
-  styleUrls: ['./categoria-cadastro.component.css']
+    selector: 'app-categoria-cadastro',
+    templateUrl: './categoria-cadastro.component.html',
+    styleUrls: ['./categoria-cadastro.component.css']
 })
 export class CategoriaCadastroComponent implements OnInit {
 
-  constructor() { }
+    categoria = new Categoria();
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private route: ActivatedRoute,
+        private router: Router,
+        private title: Title
+    ) { }
+
+    ngOnInit(): void {
+        this.title.setTitle('Nova Categoria');
+
+    }
+
+    salvar(categoriaForm: NgForm) {
+
+    }
+
+    get editando() {
+        return Boolean(this.categoria.id);
+    }
+
+    novo(categoriaForm: NgForm) {
+        categoriaForm.reset(new Categoria());
+        this.router.navigate(['/categorias/nova'])
+    }
 
 }
