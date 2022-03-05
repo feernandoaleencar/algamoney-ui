26192f166c1d0bc178f30f6cdd7c10c2d46b5002
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-04 21:09:43 -0300

    Tratando casos de erros e sucesso de autenticação

--- src/app/seguranca/login-form/login-form.component.ts
+++ src/app/seguranca/login-form/login-form.component.ts
@@ -1,25 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Seguranca} from "../../core/model";
 import {AuthService} from "../auth.service";
+import {ErrorHandlerService} from "../../core/error-handler.service";
+import {Router} from "@angular/router";
 
 @Component({
-  selector: 'app-login-form',
-  templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+    selector: 'app-login-form',
+    templateUrl: './login-form.component.html',
+    styleUrls: ['./login-form.component.css']
 })
 export class LoginFormComponent implements OnInit {
 
-  seguranca = new Seguranca();
+    seguranca = new Seguranca();
 
-  constructor(
-      private authService: AuthService,
-  ) { }
+    constructor(
+        private authService: AuthService,
+        private errorHandlerService: ErrorHandlerService,
+        private router: Router
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
-  login(usuario: Seguranca){
-    this.authService.login(usuario);
-  }
+    login(usuario: Seguranca) {
+        this.authService.login(usuario)
+            .then(() => {
+                this.router.navigate(['/lancamentos']);
+            })
+            .catch(erro =>{
+                this.errorHandlerService.handle(erro);
+            });
+    }
 
 }
