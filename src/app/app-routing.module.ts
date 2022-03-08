2442cd7c90a57451826194878bc0fb4d34fbf039
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-07 21:47:16 -0300

    Alterando path padrão para componente de login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import {PaginaNaoEncontradaComponent} from "./core/pagina-nao-encontrada.compone
 import {NgModule} from "@angular/core";
 
 const routes: Routes = [
-    {path: '', redirectTo: 'lancamentos', pathMatch: 'full'},
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
     {path: '**', redirectTo: 'pagina-nao-encontrada'},
 ];
