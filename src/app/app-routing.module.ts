2eb88d958a0e01b9c7194a802901b7dcd4b768fb
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-09 21:33:03 -0300

    Adicionando path para component nao-autorizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import {RouterModule, Routes} from "@angular/router";
 import {PaginaNaoEncontradaComponent} from "./core/pagina-nao-encontrada.component";
 import {NgModule} from "@angular/core";
+import {NaoAutorizadoComponent} from "./core/nao-autorizado.component";
 
 const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'nao-autorizado', component: NaoAutorizadoComponent},
+
+    //Não adicionar novos path abaixo
     {path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent},
     {path: '**', redirectTo: 'pagina-nao-encontrada'},
 ];
