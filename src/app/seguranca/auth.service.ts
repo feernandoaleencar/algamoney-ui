1dc908e0b04b3efd0eeff591d68767f45272b460
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-09 21:34:59 -0300

    Adicionando metodo para verificar permissão do usuário

--- src/app/seguranca/auth.service.ts
+++ src/app/seguranca/auth.service.ts
@@ -90,4 +90,14 @@ export class AuthService {
 
         return !token || this.jwtHelper.isTokenExpired(token);
     }
+
+    temQualquerPermissao(roles: any){
+        for (const role of roles) {
+            if (this.temPermissao(role)){
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
