06d9f120d8dcbed763143d1794fd6661395e429c
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-10 23:27:57 -0300

    Inserindo authguard para as rotas de pessoas e categorias

--- src/app/categorias/categorias-routing.module.ts
+++ src/app/categorias/categorias-routing.module.ts
@@ -2,11 +2,27 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {CategoriasPesquisaComponent} from "./categorias-pesquisa/categorias-pesquisa.component";
 import {CategoriaCadastroComponent} from "./categoria-cadastro/categoria-cadastro.component";
+import {AuthGuard} from "../seguranca/auth.guard";
 
 const routes: Routes = [
-    {path: 'categorias', component: CategoriasPesquisaComponent},
-    {path: 'categorias/nova', component: CategoriaCadastroComponent},
-    {path: 'categorias/:id', component: CategoriaCadastroComponent},
+    {
+        path: 'categorias',
+        component: CategoriasPesquisaComponent,
+        canActivate: [AuthGuard],
+        data: { roles: ['ROLE_PESQUISAR_CATEGORIA']}
+    },
+    {
+        path: 'categorias/nova',
+        component: CategoriaCadastroComponent,
+        canActivate: [AuthGuard],
+        data: { roles: ['ROLE_CADASTRAR_CATEGORIA']}
+    },
+    {
+        path: 'categorias/:id',
+        component: CategoriaCadastroComponent,
+        canActivate: [AuthGuard],
+        data: { roles: ['ROLE_CADASTRAR_CATEGORIA']}
+    },
 ];
 
 @NgModule({
