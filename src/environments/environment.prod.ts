3b143fdcd2075b61c6bd36c0eefe9f08340cf996
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-12 22:17:43 -0300

    Preparando deploy heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:8080',
+    apiUrl: 'https://algamoney-gen-api.herokuapp.com',
     tokenAllowedDomains: [ /algamoney-api.herokuapp.com/ ],
     tokenDisallowedRoutes: [/\/oauth\/token/],
 }
