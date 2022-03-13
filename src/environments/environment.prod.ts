c1ef84c2cf748005053b5b097e7b2735de890d49
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-03-12 23:16:47 -0300

    Corrigindo environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     apiUrl: 'https://algamoney-gen-api.herokuapp.com',
-    tokenAllowedDomains: [ /algamoney-api.herokuapp.com/ ],
+    tokenAllowedDomains: [ /algamoney-gen-api.herokuapp.com/ ],
     tokenDisallowedRoutes: [/\/oauth\/token/],
 }
