6b86d9c5628926abac952ce39a51ff59de56ecd6
Fernando Alencar <feernando_aleencar@hotmail.com>
2022-04-06 23:44:04 -0300

    Criando componente de cadastro de contato para pessoa

--- src/app/pessoas/pessoas.module.ts
+++ src/app/pessoas/pessoas.module.ts
@@ -4,11 +4,13 @@ import {PessoaCadastroComponent} from "./pessoa-cadastro/pessoa-cadastro.compone
 import {PessoasPesquisaComponent} from "./pessoas-pesquisa/pessoas-pesquisa.component";
 import {SharedModule} from "../shared/shared.module";
 import {PessoasRoutingModuleModule} from "./pessoas-routing.module";
+import { PessoaCadastroContatoComponent } from './pessoa-cadastro-contato/pessoa-cadastro-contato.component';
 
 @NgModule({
     declarations: [
         PessoaCadastroComponent,
         PessoasPesquisaComponent,
+        PessoaCadastroContatoComponent,
     ],
     imports: [
         SharedModule,
